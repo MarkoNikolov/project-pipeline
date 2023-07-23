06b43e06cd59dd11c5dbb98d17a70aeba48bdb3f
Marko Nikolov <marko.nikolov@gmail.com>
2023-07-23 20:49:21 +0300

    some fixes ecr

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,3 +1,25 @@
 resource "aws_ecr_repository" "ecr" {
   name = var.aws_ecr_repository_name
 }
+
+resource "aws_ecr_lifecycle_policy" "ecr_policy" {
+  repository = aws_ecr_repository.ecr.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        "rulePriority"    : 10,
+        "description"     : "Expire old images",
+        "selection"       : {
+          "tagStatus"        : "tagged",
+          "countType"        : "imageCountMoreThan",
+          "countNumber"      : 10,
+          "countUnit"        : "image"
+        },
+        "action"          : {
+          "type"             : "expire"
+        }
+      }
+    ]
+  })
+}
