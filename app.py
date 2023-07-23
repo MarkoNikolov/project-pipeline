15b8266fc4996baeaf915a0a0dece0b5f000a8c9
Marko Nikolov <marko.nikolov@gmail.com>
2023-07-23 20:22:20 +0300

    app change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"Hello from this demo web project :) :D >.<"
+    return f"Hello from this demo web project :) :D"
 
 if __name__ == "__main__":
          app.run(host='0.0.0.0')
