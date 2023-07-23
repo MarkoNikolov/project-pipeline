786de98e73326333decab4b9f5bac2cfb07fc441
Marko Nikolov <marko.nikolov@gmail.com>
2023-07-23 13:23:48 +0300

    little changes

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -36,6 +36,7 @@ rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum upgrade -y
 amazon-linux-extras install java-openjdk11 -y
 yum install jenkins git -y
+
 ####### DOCKER ############
 yum install -y docker
 systemctl start docker
@@ -45,9 +46,9 @@ systemctl enable jenkins
 systemctl start jenkins
 
 #### KUBECTLandHELM ######
-curl -LO https://dl.k8s.io/release/v1.23.6/bin/linux/amd64/kubectl
+curl -LO https://dl.k8s.io/release/v1.27.4/bin/linux/amd64/kubectl
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-curl -L https://git.io/get_helm.sh | bash -s -- --version v3.8.2
+curl -L https://git.io/get_helm.sh | bash -s -- --version v3.12.2
 
 ###### KIND ########
 [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
